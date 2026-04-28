f4f2859f7fd1d265c21039856065f81b5af18cf0
cbig3 <romanvillagran386@gmail.com>
2026-04-27 19:00:01 -0700

    added the data folder as well as the Webdevacademycontext fille and also added the code to it

---
+++ COMP003B.LectureActivity6/Data/WebDevAcademyContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.LectureActivity6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.LectureActivity6.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options) : base(options) { 
+        }
+
+        public DbSet<Student> students { get; set; }
+
+        public DbSet<Course> courses { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
